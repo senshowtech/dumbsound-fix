727db8d37c25af6669ac2a58764fdb9289cbda55
sena <hargiantofebrisena@gmail.com>
2022-05-30 19:27:20 +0700

    initial

--- src/component/Home/Card.js
+++ src/component/Home/Card.js
@@ -4,7 +4,6 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
 import CardActions from "@mui/material/CardActions";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
